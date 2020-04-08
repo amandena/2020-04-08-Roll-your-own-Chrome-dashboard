bf7cbed15cad5e9131f0ed47342c7a8d6bfa7fb9
amandena <manda.panda91@yahoo.com>
2020-04-08 14:49:53 -0400

    added content for testing

--- chrome-extension/src/App.js
+++ chrome-extension/src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <html>
+        <body>
+          <h1>Random Image of the Moment:</h1>
+          <img src="https://source.unsplash.com/random" alt=""/>
+        </body>
+      </html>
     </div>
   );
 }
